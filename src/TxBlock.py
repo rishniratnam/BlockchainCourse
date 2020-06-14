125819c1560380ec7de2935c94f09de712fb5717
Rishni Ratnam <rishni_ratnam@mcksiney.com>
2020-06-14 10:06:04 +1000

    Added TxBlock

---
+++ src/TxBlock.py
@@ -0,0 +1,36 @@
+from Blockchain import CBlock
+from Signatures import generate_keys, sign, verify
+from Transaction import Tx
+import pickle
+
+class TxBlock (CBlock):
+    def __init__(self, previousBlock):
+        pass
+    def addTx(self, Tx_in):
+        pass
+    def is_valid(self):
+        return False
+
+if __name__ == "__main__":
+    pr1, pu1 = generate_keys()
+    pr2, pu2 = generate_keys()
+    pr3, pu3 = generate_keys()
+
+    Tx1 = Tx()
+    Tx1.add_input(pu1,1)
+    Tx1.sign(pr1)
+
+    print(Tx1.is_valid())
+
+    message = b"Some text"
+    sig = sign(message,pr1)
+
+    savefile = open("save.dat", "wb")
+    pickle.dump(Tx1, savefile)
+    savefile.close()
+
+    loadfile = open("save.dat", "rb")
+    newTx = pickle.load(loadfile)
+
+    print(newTx.is_valid())
+
